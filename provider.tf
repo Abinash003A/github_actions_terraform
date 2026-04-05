56b9b8e186396492f2afb9388083b938263eca80
Abinash <abinashsubudhi213@gmail.com>
2026-04-05 23:10:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">4.0" #morthan 4.0 version
+      version = ">4.0" #more than 4.0 version
     }
   }
 }
